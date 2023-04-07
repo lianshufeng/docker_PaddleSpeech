2a39f573910cac530449afa22a371a0e562db5ab
lianshufeng <251708339@qq.com>
2023-04-07 13:08:15 +0800

    submit

--- script/install.sh
+++ script/install.sh
@@ -5,6 +5,12 @@ apt update ; apt-get install libsndfile1 -y ; rm -rf /var/lib/apt/lists/*
 pip install soundfile
 
 
+
 # PaddleSpeech
 #cd  /tmp ; git clone https://github.com/paddlepaddle/PaddleSpeech ; cd PaddleSpeech ; pip install -e .  ; rm -rf /tmp/PaddleSpeech
 
+
+
+# test 
+paddlespeech tts --input "你好，欢迎使用百度飞桨深度学习框架！" --output /tmp/output.wav
+rm -rf /tmp/output.wav
